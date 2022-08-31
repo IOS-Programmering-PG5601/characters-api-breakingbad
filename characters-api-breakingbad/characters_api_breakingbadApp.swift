a684139596c4e5e02e2594ecfbfc8b179f6311bf
Stian <stian.oek@gmail.com>
2022-08-31 21:47:33 +0200

    Initial Commit

---
+++ characters-api-breakingbad/characters_api_breakingbadApp.swift
@@ -0,0 +1,17 @@
+//
+//  characters_api_breakingbadApp.swift
+//  characters-api-breakingbad
+//
+//  Created by Stian Ihler Hermansen on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct characters_api_breakingbadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
